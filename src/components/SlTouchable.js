93d079066ed516278de733c25706f2286c5c982b
VincentBel <buaazqh@gmail.com>
2016-05-09 15:05:16 +0800

    [SlTouchable] Custom Touchable Component

---
+++ src/components/SlTouchable.js
@@ -0,0 +1,26 @@
+import React, {
+  PropTypes,
+  TouchableNativeFeedback,
+} from 'react-native'
+import { rippleColor as defaultRippleColor } from '../theme'
+
+
+// Custom Touchable Component
+function SlTouchable({ children, onPress, rippleColor = defaultRippleColor }) {
+  return (
+    <TouchableNativeFeedback
+      onPress={onPress}
+      background={TouchableNativeFeedback.Ripple(rippleColor)}
+    >
+      {children}
+    </TouchableNativeFeedback>
+  )
+}
+
+SlTouchable.propTypes = {
+  children: PropTypes.node,
+  onPress: PropTypes.func,
+  rippleColor: PropTypes.string,
+}
+
+export default SlTouchable
