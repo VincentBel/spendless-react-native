cd1f6b85422fb30b5b2abaa124b58aa7aed7170c
VincentBel <buaazqh@gmail.com>
2016-05-24 19:13:30 +0800

    Filter out redux-form error in centralizedError

--- src/reducers/centralizedError.js
+++ src/reducers/centralizedError.js
@@ -2,8 +2,9 @@ const RESET_CENTRALIZED_ERROR = 'RESET_CENTRALIZED_ERROR'
 
 export default function reducer(state = null, action = {}) {
   const { type, error, customizeErrorHandling } = action
-
-  if (type === RESET_CENTRALIZED_ERROR) {
+  if (type === RESET_CENTRALIZED_ERROR ||
+    type.indexOf('redux-form') !== -1 // filter out redux-form error
+  ) {
     return null
   } else if (!customizeErrorHandling && error) {
     // 只有在没有声明自行处理错误 并且 有错误时, 才记录错误
